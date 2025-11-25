50d773154ac517b6ee823e54c8f650365285b246
Manuel Gjergji <manuel.gjergji@marconirovereto.it>
2025-11-25 09:32:30 +0000

    Aggiunta dei file HTML, CSS e JS per la pagina iniziale con contenuti e stili di base.

---
+++ HTML, CSS, JS/main.js
@@ -0,0 +1,12 @@
+// Mostra un messaggio quando si apre la pagina
+alert("Benvenuto nella mia pagina!");
+
+// Chiede il nome e lo scrive nella console
+let nome = prompt("Come ti chiami?");
+console.log("Ciao " + nome + "!");
+
+// Funzione collegata a un bottone
+function saluta() {
+    alert("Hai cliccato il bottone!");
+}
+
